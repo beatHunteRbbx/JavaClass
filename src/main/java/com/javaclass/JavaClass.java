70bfebf9344d12fb0649800769f4cf648d779083
beathunter <nikitasherepa@gmail.com>
2019-02-13 22:24:11 +0300

    test commit from IntelliJ Idea

--- src/main/java/com/javaclass/JavaClass.java
+++ src/main/java/com/javaclass/JavaClass.java
@@ -2,6 +2,6 @@ package com.javaclass;
 
 public class JavaClass {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Wassup!");
     }
 }
