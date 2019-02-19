f0cf2d0bee9657f5f1ae030aac41a8ded1264fef
beathunter <nikitasherepa@gmail.com>
2019-02-19 22:06:20 +0300

    fixed foolCheck() and calculator()

--- src/main/java/com/javaclass/NumberDimension.java
+++ src/main/java/com/javaclass/NumberDimension.java
@@ -1,30 +1,30 @@
 package com.javaclass;
 
-import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 
-import java.util.Scanner;
 
 public class NumberDimension {
 
-    public static String foolCheck(String str) {
-        if (str.matches("\\s*\\d+\\s*\\w{1,3}\\s*(\\+|\\-|\\*|\\\\){1}\\s*\\d+\\s*\\w{1,3}\\s*")) {
+    public static String foolCheck( String str) {
+        if (str.matches("\\s*\\d+\\s*\\w{1,3}\\s*(\\+|\\-|\\*|/){1}\\s*\\d+\\s*\\w{1,3}\\s*")) {
             String userString = str.replaceAll("\\s+", " ").trim();
             return userString;
         }
         else return "Invalid input! Try again. Example: 1 km + 1 cm";
     }
-    public static int calculator(String str) {
+    public static double calculator(String str) {
         String[] userString = foolCheck(str).replaceAll("\\s+", " ").trim().split(" ");
         int userNumber1 = Integer.parseInt(userString[0]);
         String userDimension1 = userString[1];
-        String operation = userString[2];
+        String sign = userString[2];
         int userNumber2 = Integer.parseInt(userString[3]);
         String userDimension2 = userString[4];
-        return userNumber1 + userNumber2;
+        Dimension dimension = new Dimension();
+        double summa = dimension.operation(userNumber1, userDimension1, sign, userNumber2, userDimension2);
+        return summa;
     }
     public static void main(String[] args) {
 
-        System.out.println(calculator("1123   km     + 2                   cm          "));
+        System.out.println(calculator("1123   km     +2                   cm          "));
         /*System.out.println("Example: 1 cm + 2 km");
         System.out.print("Input an expression as in the example: ");
         String inputData1 = foolCheck();
