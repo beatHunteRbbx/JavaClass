fbc93b9c34f2d63e6f7ed26b52bdead08d0733b2
beathunter <nikitasherepa@gmail.com>
2019-02-21 22:41:07 +0300

    Deleted 2 HashMaps: length, time Change algorithm to switch idea. Created SI() method to converted dimensions to SI system.

--- src/main/java/com/javaclass/Dimension.java
+++ src/main/java/com/javaclass/Dimension.java
@@ -6,24 +6,36 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Dimension {
+
+    public double SI (String dimension, double number) {
+        switch (dimension) {
+            case "km": case "км":
+                number *= 1000.0;
+                break;
+            case "s": case "с": case "m": case "м":
+                number *= 1.0;
+                break;
+            case "dm": case "дм":
+                number *= 0.1;
+                break;
+            case "cm": case "см":
+                number *= 0.01;
+                break;
+            case "ms": case "мс": case "mm": case "мм":
+                number *= 0.001;
+                break;
+            case "mks": case "мкс": case "mkm": case "мкм":
+                number *= 0.000001;
+                break;
+            case "ns": case "нс": case "nm": case "нм":
+                number *= 0.000000001;
+        }
+        return number;
+    }
     public double operation(double number1, String dimension1, String sign, double number2, String dimension2) {
-        HashMap<Pair<String, String>, Double> length = new HashMap<>();
-        length.put(new Pair<>("km","км"), 1000.0);
-        length.put(new Pair<>("m","м"), 1.0);
-        length.put(new Pair<>("dm","дм"), 0.1);
-        length.put(new Pair<>("cm","см"), 0.01);
-        length.put(new Pair<>("mm","мм"), 0.001);
-        length.put(new Pair<>("mkm","мкм"), 0.000001);
-        length.put(new Pair<>("nm","нм"), 0.000000001);
-
-        HashMap<Pair<String, String>, Double> time = new HashMap<>();
-        time.put(new Pair<>("s","с"), 1.0);
-        time.put(new Pair<>("ms","мс"), 0.001);
-        time.put(new Pair<>("mks","мкс"), 0.000001);
-        time.put(new Pair<>("ns","нс"), 0.000000001);
-
+        number1 = SI(dimension1, number1);
+        number2 = SI(dimension2, number2);
         double result = 0;
-        if (dimension1.equals(dimension2)) {
             switch (sign) {
                 case "+":
                     result = number1 + number2;
@@ -39,25 +51,6 @@ public class Dimension {
                     break;
                 default:
                     System.out.println("Invalid operation.");
-            }
-        } else {
-            switch (sign) {
-                case "+":
-                    //нужно как-то по паре достать значение из мапы
-                    result = number1 * length.get(dimension1) + number2 * length.get(dimension1);
-                    break;
-                case "-":
-                    result = number1 - number2;
-                    break;
-                case "*":
-                    result = number1 * number2;
-                    break;
-                case "/":
-                    result = number1 / number2;
-                    break;
-                default:
-                    System.out.println("Invalid operation.");
-            }
         }
         return result;
     }
