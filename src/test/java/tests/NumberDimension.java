77ee9b9bcfeeb51f2786e6d28416edfb294f2b65
beathunter <nikitasherepa@gmail.com>
2019-02-22 09:50:10 +0300

    trying to make tests...

---
+++ src/test/java/tests/NumberDimension.java
@@ -0,0 +1,15 @@
+package tests;
+
+import com.javaclass.Dimension;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+public class NumberDimension {
+
+    @Test
+    public void calculator() {
+        Dimension dimension = new Dimension("1   mks    + 1                   s         ")
+    }
+}
