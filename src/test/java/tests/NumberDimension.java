2681e89588c6a5d5112ee0ee48582f7bd1989417
beathunter <nikitasherepa@gmail.com>
2019-02-22 23:20:10 +0300

    comment test

--- src/test/java/tests/NumberDimension.java
+++ src/test/java/tests/NumberDimension.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.Test;
 
 public class NumberDimension {
 
-    @Test
+    /*@Test
     public void calculator() {
         Dimension dimension = new Dimension("1   mks    + 1                   s         ")
-    }
+    }*/
 }
