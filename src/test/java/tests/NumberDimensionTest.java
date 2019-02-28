d0fac561236b811a9d6cf639435bbcc4c68c9844
beatHunteRbbx <nikitasherepa@gmail.com>
2019-02-28 14:35:13 +0300

    Fixed tests

--- src/test/java/tests/NumberDimensionTest.java
+++ src/test/java/tests/NumberDimensionTest.java
@@ -2,17 +2,27 @@ package tests;
 
 import com.javaclass.NumberDimension;
 import org.junit.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Assertions;
 
 
 public class NumberDimensionTest {
     @Test
     public void plusTest() {
+        NumberDimension expected = new NumberDimension(30, "кг");
         NumberDimension a = new NumberDimension(10, "кг");
         NumberDimension b = new NumberDimension(20, "кг");
-        int c = a.plus(b);
-        assertEquals(30, c);
+        NumberDimension result = a.plus(b);
+        Assertions.assertEquals(expected, result);
     }
 
-
+    @Test
+    public void minusTest() {
+        NumberDimension expected = new NumberDimension(10, "кг");
+        NumberDimension a = new NumberDimension(10, "кг");
+        NumberDimension b = new NumberDimension(20, "кг");
+        NumberDimension result = b.minus(a);
+        Assertions.assertEquals(expected, result);
+    }
 }
