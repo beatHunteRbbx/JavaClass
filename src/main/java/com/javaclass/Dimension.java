f21fadf19d1b5c681fbedd9e549e8ee5c662ec98
beathunter <nikitasherepa@gmail.com>
2019-03-03 10:00:45 +0300

    done conversion to SI system

--- src/main/java/com/javaclass/Dimension.java
+++ src/main/java/com/javaclass/Dimension.java
@@ -3,8 +3,73 @@ package com.javaclass;
 import javafx.util.Pair;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.regex.Matcher;
 
 public class Dimension {
+
+    private final int number;
+    private final String dimension;
+
+    public Dimension(int userNumber, String userDimension) {
+        number = userNumber;
+        dimension = userDimension;
+    }
+
+    private String getDimension() { return dimension; }
+    private int getNumber() { return number; }
+
+    public Dimension toSI() {
+        int userNumber = getNumber();
+        String userDimension = getDimension();
+
+        switch (userDimension.toLowerCase()) {
+        case "km": case "км":
+            userNumber *= 1000.0;
+            break;
+        case "s": case "с": case "m": case "м": case "kg": case "кг":
+            userNumber *= 1.0;
+            break;
+        case "dm": case "дм":
+            userNumber *= 0.1;
+            break;
+        case "cm": case "см":
+            userNumber *= 0.01;
+            break;
+        case "ms": case "мс": case "mm": case "мм": case "g": case "г":
+            userNumber *= 0.001;
+            break;
+        case "mks": case "мкс": case "mkm": case "мкм": case "mg": case "мг":
+            userNumber *= 0.000001;
+            break;
+        case "ns": case "нс": case "nm": case "нм":
+            userNumber *= 0.000000001;
+        case "h": case "ч":
+            userNumber *= 3600.0;
+            break;
+        case "min": case "мин":
+            userNumber *= 60.0;
+            break;
+        case "d": case "д":
+            userNumber *= 86400.0;
+            break;
+        }
+
+        if (userDimension.toLowerCase().matches("km|m|dm|cm|mm|mkm|nm"))
+            userDimension = "m";
+        else if (userDimension.toLowerCase().matches("км|м|дм|см|мм|мкм|нм"))
+            userDimension = "м";
+        else if (userDimension.toLowerCase().matches("d|h|min|s|mc|mks|ns"))
+            userDimension = "s";
+        else if (userDimension.toLowerCase().matches("д|ч|мин|c|мс|мкс|нс"))
+            userDimension = "с";
+        else if (userDimension.toLowerCase().matches("кг|г|мг"))
+            userDimension = "кг";
+        else if (userDimension.toLowerCase().matches("kg|g|mg"))
+            userDimension = "kg";
+
+        return new Dimension(userNumber, userDimension);
+    }
+
 
 
     /*private double SI (String dimension, double number) {
