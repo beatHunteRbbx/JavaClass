09bd754fa59afa03e7acd5d9a0bd9d0b405bc98a
beathunter <nikitasherepa@gmail.com>
2019-03-11 19:00:25 +0300

    add some stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,11 @@ import com.javaclass.NumberDimension;
 
 public class Main {
     public static void main(String[] args) {
-        Dimension c = new Dimension(20, "кг");
-        NumberDimension a = new NumberDimension(10, "кг");
+        Dimension c = new Dimension(1, "г");
+        Dimension d = new Dimension(10, "кг");
+        NumberDimension a = new NumberDimension(1, "г");
         NumberDimension b = new NumberDimension(20, "кг");
-        System.out.println();
+        NumberDimension res = a.toSI().plus(b.toSI());
+        System.out.println(res);
     }
 }
